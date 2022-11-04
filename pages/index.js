7f9a293544c46210b08408e4cbfdfbd60fec70ab
bweam <supatachbeam@gmail.com>
2022-11-04 14:49:17 +0900

    wip: loading animation

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,51 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import dynamic from 'next/dynamic'
-import { useEffect } from 'react'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import dynamic from "next/dynamic";
+import { useEffect } from "react";
+import Link from "next/link";
 
 export default function Home() {
   useEffect(() => {
     const menu = document.getElementById("menu");
 
-    Array.from(document.getElementsByClassName("menu-item"))
-      .forEach((item, index) => {
+    Array.from(document.getElementsByClassName("menu-item")).forEach(
+      (item, index) => {
         item.onmouseover = () => {
           menu.dataset.activeIndex = index;
           console.log(index);
-        }
-      });
-  })
+        };
+      }
+    );
+  });
   // const DynamicComponentWithNoSSR = dynamic(() => import('../components/List'), {
   //   ssr: false
   // })
 
   return (
-    <div id='menu'>
-      <div id='menu-items'>
-        <a href='/' className='menu-item'>Generate Fit</a>
-        <a href='/' className='menu-item'>Add Clothes</a>
-        <a href='/' className='menu-item'>Credits</a>
+    <div className="bg">
+      <div className='loading-layer'>
+        <div className='title-box'>
+          <div className="title">
+            Fit Generator
+          </div>
+        </div>
       </div>
-      <div id='background-pattern'/>
-      <div id='background-img'/>    
+      <div id="menu">
+        <div id="menu-items">
+          <Link href="/" className="menu-item">
+            Generate Fit
+          </Link>
+          <Link href="/" className="menu-item">
+            Add Clothes
+          </Link>
+          <Link href="/" className="menu-item">
+            Credits
+          </Link>
+        </div>
+        <div id="background-pattern" />
+        <div id="background-img" />
+      </div>
     </div>
-  )
+  );
 }
